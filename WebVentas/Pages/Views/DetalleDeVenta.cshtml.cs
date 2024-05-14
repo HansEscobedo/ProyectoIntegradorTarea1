8f2fe322a920b8dcd84a85d2e7060eebb5f91754
Friedpotatu <nicolas.pavez@alumnos.ucn.cl>
2024-05-14 02:06:44 -0400

    update: sistema de ventas

--- WebVentas/Pages/Views/DetalleDeVenta.cshtml.cs
+++ WebVentas/Pages/Views/DetalleDeVenta.cshtml.cs
@@ -9,10 +9,12 @@ namespace libreria.Pages
     public class DetalleDeVentaModel : PageModel
     {
         public List<Libro> Libros { get; set; }
-        public void OnGet()
+        public int idVenta { get; set; }
+        public void OnGet(int idVenta)
         {
             ContextoLibreria contexto = new ContextoLibreria();
             Libros = contexto.Libros.ToList();
+            this.idVenta = idVenta;
         }
 
         public RedirectToPageResult OnPost(int idVenta, int idLibro, int cantidad)
@@ -22,10 +24,15 @@ namespace libreria.Pages
             Venta venta = contexto.Ventas.Find(idVenta);
             Libro libro = contexto.Libros.Find(idLibro);
             DetalleVenta detalle = new DetalleVenta();
+            venta.Total += libro.Precio * cantidad;
+            libro.Cantidad -= cantidad;
             detalle.libro = libro;
             detalle.Cantidad = cantidad;
             detalle.venta = venta;
+            detalle.IdLibro = libro.Id;
+            detalle.IdVenta = venta.Id;
             contexto.DetalleVentas.Add(detalle);
+            contexto.Ventas.Update(venta);
             contexto.SaveChanges();
             return RedirectToPage("/Views/DetalleDeVenta", new { idVenta = idVenta });
         }
--- WebVentas/Pages/Views/ListarVentas.cshtml.cs
+++ WebVentas/Pages/Views/ListarVentas.cshtml.cs
@@ -1,12 +1,17 @@
+using libreria.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyApp.Namespace
+namespace libreria.pages
 {
     public class ListarVentasModel : PageModel
     {
+        public List<Venta> Ventas { get; set; }
         public void OnGet()
         {
+            ContextoLibreria contexto = new ContextoLibreria();
+            Ventas = contexto.Ventas.ToList();
+
         }
     }
 }
